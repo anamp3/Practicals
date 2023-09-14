81704d327b61c599f7a367cb71efdf82be1b1290
Anam Philisani <s224103121@mandela.ac.za>
2023-09-14 12:30:03 +0200

    fixed the writing object

--- Practical_Semester_Test/files.py
+++ Practical_Semester_Test/files.py
@@ -1,24 +1,32 @@
 import csv
 
 FILE = "sales.csv"
- 
-# with open('imported_files.txt', 'r') as f:
-#     imported_files = f.readlines()
 
-# if FILE + '\n' in imported_files:
-#     print('This file has already been imported.')
+def Append_TextFile():
+    with open('imported_files.txt', 'a') as file:
+        file.write(FILE+ '\n')
+
+def Read_TextFile():
+    with open('imported_files.txt', 'r') as f:
+        imported_files = f.readlines()
+
+    if FILE+ '\n' in imported_files:
+        print('This file has already been imported.')
+    else:
+        print('This file has not been imported yet.')
+
 
 
 '''This is the input that will be from the user'''
 def Write_Sales(sales):
     with open(FILE, 'w', newline="") as file:
         write = csv.writer(file)
-        write.writerows(file)
+        write.writerows(sales)
 
 def Read_Sales():
     sales = [[]]
     with open(FILE, 'r', newline="")as file:
         read = csv.reader(file)
-        for line in read:
+        for line in read: 
             sales.append(line)
     return sales
--- Practical_Semester_Test/sales.py
+++ Practical_Semester_Test/sales.py
@@ -1,4 +1,5 @@
 import files
+import csv
 
 def Add_Sales_Data(sales):
     amount = float(input('Amount:\t\t'))
@@ -6,8 +7,9 @@ def Add_Sales_Data(sales):
     month = int(input('Month (1-12):\t'))
     day = int(input('Day (1-31):\t'))
 
-    quarter = (month - 1) // 3 + 1
-    sale = [year, month, day, quarter, amount]
+    Quarter(month)
+
+    sale = [year, month, day, amount]
     sales.append(sale)
 
     files.Write_Sales(sales)
@@ -15,20 +17,24 @@ def Add_Sales_Data(sales):
     print(f"Sales for {year}-{month}-{day} added.")
 
 
+def Quarter(month):
+    quarter = (month - 1) // 3 + 1
+    return quarter
+
 def Import_Sales(sales):
+    file_import = input("Enter file name to import: ")
     print("\tDate\tQuater\tAmount")
     print("__________________________________________")
-    for i, sale in enumerate(sales, start=1):
-        if sale[0] or sale[1] or sale[2] or sale[3] or sale[4] == 0:
-            print(f"{i}.\t{sale[0]}-{sale[1]}-{sale[2]}\t{sale[3]}\t{sale[4]}")
-            totalValue += sale[4]
-        else:
-            print(f"{i}.\t{sale[0]}-{sale[1]}-{sale[2]}\t{sale[3]}\t{sale[4]}")
-            totalValue += sale[4]
+    with open(file_import, "r", newline="") as imported:
+        line = csv.reader(imported)
+        for i, sale in enumerate (line, start=1):
+            print(f"{i}.\t")
+
+
     
     
 
-
+#maybe in the future store this in a separate module
 def Command_Menu():
     print("SALES DATA IMPORTER")
     print()
